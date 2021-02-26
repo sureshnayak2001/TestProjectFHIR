0f14e2dfda14bf829d4ff4969885e77b54b5489a
Kinjal Jani <Kinjal Jani@DESKTOP-OKSD2NT.hitronhub.home>
2021-02-25 23:10:31 -0500

    Commit 1.0.0

--- io.hapifhir.playground/src/main/java/io/hapifhir/playground/SampleClient.java
+++ io.hapifhir.playground/src/main/java/io/hapifhir/playground/SampleClient.java
@@ -24,18 +24,13 @@ public class SampleClient {
 	public static void main(String[] args) {
 
 		StringBuffer sb = new StringBuffer();
-
-		getPatientinfo("SMITH", sb);
-
-		// outputlist.stream().sorted((o1,
-		// o2)->o1.getFamily().compareTo(o2.getFamily())).forEach(s->System.out.println(
-		// "last name"+s.getFamily()+"first name"+s.getGiven()));
+		List<Patient> patientsresp = new ArrayList<Patient>();
+		getPatientinfo("SMITH", sb, patientsresp );		
+		patientsresp.stream().sorted((o1,o2)->o1.getNameFirstRep().getFamily().compareTo(o2.getNameFirstRep().getFamily())).forEach(s->System.out.println("  last name  "+s.getNameFirstRep().getFamily()+" first name "+s.getNameFirstRep().getGiven()+ "   Birth Date   " + s.getBirthDate()));
 
 		String fileName = "src/test/resources/lines.txt";
-
 		List<String> list = new ArrayList<String>();
 		try (BufferedReader br = Files.newBufferedReader(Paths.get(fileName))) {
-
 			// br returns as stream and convert it into a List
 			list = br.lines().collect(Collectors.toList());
 
@@ -43,47 +38,37 @@ public class SampleClient {
 			e.printStackTrace();
 		}
 		long startTime = System.nanoTime();
-
-		for (String s : list) {
-
-			System.out.println("---  " + s);
-			getPatientinfo(s, sb);
+		List<Patient> patientsresparray = new ArrayList<Patient>();
+		for (String s : list) {			
+			getPatientinfo(s, sb, patientsresparray);
 		}
 		long estimatedTime = System.nanoTime() - startTime;
+		System.out.println("  Time taken to get response" + estimatedTime);
+		System.out.println(sb);		
+		//patientsresparray.stream().sorted((o1,o2)->o1.getNameFirstRep().getFamily().compareTo(o2.getNameFirstRep().getFamily())).forEach(s->System.out.println("  last name  "+s.getNameFirstRep().getFamily()+" first name "+s.getNameFirstRep().getGiven()+ "   Birth Date   " + s.getBirthDate()));
 
-		System.out.println("  Time taken" + estimatedTime);
-		System.out.println(sb);
 
 	}
 
-	public static void getPatientinfo(String name, StringBuffer sb) {
+	public static void getPatientinfo(String name, StringBuffer sb , List<Patient> patientsresp ) {
 		List<IBaseResource> patients = new ArrayList<>();
-
 		// Create a FHIR client
 		FhirContext fhirContext = FhirContext.forR4();
 		IGenericClient client = fhirContext.newRestfulGenericClient("http://hapi.fhir.org/baseR4");
 		client.registerInterceptor(new LoggingInterceptor(false));
-
 		// Search for Patient resources
 		Bundle response = client.search().forResource("Patient").where(Patient.FAMILY.matches().value(name))
 				.returnBundle(Bundle.class).execute();
 		patients.addAll(BundleUtil.toListOfResources(fhirContext, response));
-
+		
 		for (IBaseResource ibaseResource : patients) {
-			Patient p = (Patient) ibaseResource;
-			// System.out.println(" Birth Date " + p.getBirthDate());
-
+			Patient p = (Patient) ibaseResource;			
+			patientsresp.add(p);
 			List<HumanName> humanName = p.getName();
-			for (HumanName h : humanName) {
-				// System.out.println("Last Name" + h.getFamily());
-				// System.out.println("First Name" + h.getGiven());
-
+			for (HumanName h : humanName) {				
 				sb.append(" Birth Date " + p.getBirthDate() + " Last Name " + h.getFamily() + " First Name"
 						+ h.getGiven());
-
 			}
-		}
-
+		}		
 	}
-
 }
